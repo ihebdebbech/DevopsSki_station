504da38896679539b39aeb66cea0576d5a9e93c3
mohamed chebbi <chebbim4@gmail.com>
2024-11-09 14:28:21 +0100

    trying to fix 6.0

--- src/test/java/tn/esprit/spring/controllers/SkierRestControllerTest.java
+++ src/test/java/tn/esprit/spring/controllers/SkierRestControllerTest.java
@@ -2,8 +2,10 @@ package tn.esprit.spring.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
