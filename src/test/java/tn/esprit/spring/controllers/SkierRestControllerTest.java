ae8d62bca3d481c63819842eb5162e2312a2d305
mohamed chebbi <chebbim4@gmail.com>
2024-11-09 14:56:34 +0100

    trying to fix 6.0

--- src/test/java/tn/esprit/spring/controllers/SkierRestControllerTest.java
+++ src/test/java/tn/esprit/spring/controllers/SkierRestControllerTest.java
@@ -2,7 +2,6 @@ package tn.esprit.spring.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
